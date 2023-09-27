f52188c83cd985ff99bc3bb11931f0ec8989b5cd
Moraish Kapoor <moraish.kapoor@hotmail.com>
2023-09-27 17:58:01 +0530

    added functionality to serve json

--- myApp.js
+++ myApp.js
@@ -1,39 +1,32 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
+
+// console.log('Hello World!');
+
+// CHALLENGE 1
+// app.get("/", function(req, res) {
+//     res.send('Hello Express')
+// })
+
+// CHALLENGE 2 - SERVING STATIC ASSETS
+// app.get("/", function(req, res) {
+//     htmlFile = __dirname + '/views/index.html'
+//     res.sendFile(htmlFile);
+// })
+
+// app.use("/public", express.static(__dirname + '/public'));
 
 
+// SERVING JSON and using the .ENV FILE
 
-
-
-
-
-
-
-console.log('Hello World!');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/json', function(req, res) {
+    res_json = {"message": "Hello json"};
+    message_style = process.env.MESSAGE_STYLE;
+    if(message_style == 'uppercase') {
+        res_json['message'] = "HELLO JSON"
+    }
+    res.json(res_json);
+})
 
  module.exports = app;
